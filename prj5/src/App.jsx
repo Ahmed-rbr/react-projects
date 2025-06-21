a9893f13e380c7711c2e3d16c3ff15e682ca5b4f
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-06-21 13:54:48 +0100

    fixing bugs

--- prj5/src/App.jsx
+++ prj5/src/App.jsx
@@ -5,55 +5,73 @@ import axios from "axios";
 function App() {
   const [loading, setLoading] = useState(false);
   const [err, setError] = useState(null);
-  const [limit, setLimit] = useState(10);
+  const [count, setCount] = useState(0);
   const [data, setData] = useState([]);
   const [total, setTotal] = useState(null);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        `https://dummyjson.com/products?limit=${10}&skip=${count * 10}`
+      );
+      console.log(res);
+      if (res) {
+        setData((prev) => [...prev, ...res.data.products]);
+        setTotal(res.data.total);
+      }
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    setError(null);
-    setLoading(true);
+    fetchData();
+  }, [count]);
 
-    axios
-      .get(`https://dummyjson.com/products?limit=${limit}`)
-      .then((data) => {
-        setData(data.data.products);
-        setTotal(data.data.total);
-
-        setLoading(false);
-      })
-      .catch((e) => {
-        setError(e.message);
-        setLoading(false);
-      });
-  }, [limit]);
   return (
     <div className="w-4/5 flex flex-col justify-center items-center gap-4 mx-auto">
-      {loading && (
-        <div className="w-8 h-8 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
+      {loading ? (
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-auto my-8 gap-4">
+          {[...Array(10)].map((_, id) => (
+            <div
+              key={`skl-${id}`}
+              className="animate-pulse flex flex-col gap-2 p-4 bg-gray-200 rounded h-60 w-full shadow-xl"
+            >
+              <div className="bg-gray-300 h-32 w-full rounded"></div>
+              <div className="bg-gray-300 h-4 w-3/4 rounded"></div>
+              <div className="bg-gray-300 h-4 w-1/2 rounded"></div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className=" w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-auto my-8 gap-4">
+          {data &&
+            data.length > 0 &&
+            data.map((product) => (
+              <Card
+                key={product.id}
+                productPrice={product.price}
+                productTitle={product.title}
+                productImg={product.thumbnail}
+              />
+            ))}
+        </div>
+      )}
+
+      {!loading && !err && data.length < total && (
+        <button
+          disabled={loading}
+          onClick={() => setCount((prev) => prev + 1)}
+          className="px-6 my-6 py-2 bg-blue-400 rounded hover:cursor-pointer disabled:opacity-50"
+        >
+          {loading ? "Loading..." : "Load More"}
+        </button>
       )}
       {err && <p className="text-red-500">Error: {err}</p>}
 
-      <div className=" w-full grid grid-cols-3 mx-auto my-8 gap-4">
-        {data &&
-          data.length > 0 &&
-          data.map((product) => (
-            <Card
-              key={product.id}
-              productPrice={product.price}
-              productTitle={product.title}
-              productImg={product.thumbnail}
-            />
-          ))}
-      </div>
-      {!loading && !err && limit < total && (
-        <button
-          onClick={() => setLimit((prev) => prev + 10)}
-          className="px-6 my-6 py-2 bg-blue-400 rounded hover:cursor-pointer"
-        >
-          Load More
-        </button>
-      )}
-      {limit >= total && <p>All products loaded.</p>}
+      {data.length >= total && !err && <p>All products loaded.</p>}
     </div>
   );
 }
